a2dc99d94ed4d511ee9e0c3627cf560964c51575
mharish20 <mharish@karunya.edu.in>
2025-04-24 07:03:22 +0530

    Add hourglass pattern generation script

---
+++ Python/Patterns/HourGlass.py
@@ -0,0 +1,11 @@
+n=int(input())
+for i in range(n):
+    print(" "*i,end="")
+    for j in range(i+1,n+1):
+        print(j,end=" ")
+    print()
+for i in range(n-2,-1,-1):
+    print(" "*i ,end="")
+    for j in range(i+1,n+1):
+        print(j,end=" ")
+    print()
