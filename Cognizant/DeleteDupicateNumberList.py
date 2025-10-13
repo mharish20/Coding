30e3c9d76342f9397d274a3ef5c3040266543306
M Harish <mharish@karunya.edu.in>
2025-10-13 16:27:23 +0530

    Add alternative implementation for removing duplicate numbers using list comprehension

--- Cognizant/DeleteDupicateNumberList.py
+++ Cognizant/DeleteDupicateNumberList.py
@@ -5,4 +5,12 @@ for i in n:
     hash_map[i] = hash_map.get(i,0)+1
 for key,values in hash_map.items():
     res.append(key)
-print(res)
+print(res) 
+"""
+n = [2,4,5,2,1,2,4,6,8,3,5,7,8,4,6]
+result = []
+for i in n:
+    if i not in result:
+        result.append(i)
+print(result) 
+""" 
